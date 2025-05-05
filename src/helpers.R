730fcf00e4fe945ec70e77bc27c2272a765fae97
Alvaro Cabana <almadana@gmail.com>
2025-05-05 17:11:33 -0300

    traducción imágenes Navarro comenzada

---
+++ src/helpers.R
@@ -0,0 +1,26 @@
+# print an image
+
+makeEps <- function( file, width, height ) {
+
+	dev.print(
+		device = postscript,
+		file = file,
+		width = width,
+		height = height,
+		horizontal = FALSE
+	)
+
+}
+makePng <- function(file, width, height, res = 300) {
+  dev.print(
+    device = grDevices::png,
+    file = file,
+    width = width,
+    height = height,
+    units = "in",
+    res = res
+  )
+}
+
+
+
---
+++ src/navarro_plots_español.r
@@ -0,0 +1,116 @@
+directory <- "./imgs/navarro_img/"
+source( "./src/helpers.R" )
+probImg <- list()
+emphCol <- rgb(0,0,1)
+emphColLight <- rgb(.5,.5,1)
+emphGrey <- grey(.5)
+
+png <- TRUE
+colour <- TRUE
+
+
+# Capítulo 4 --------------
+
+## fig-4FreqProb
+set.seed(42)
+
+frequentistProb <- function() {
+
+  # needed for printing
+  fileName <- "frequentistProb_es.png"
+  width <- 12
+  height <- 8
+
+  def.par <- par(no.readonly = TRUE)
+  layout( matrix(1:4,2,2) )
+  for( i in 1:4 ) {
+
+    X <- as.numeric( runif(1000) > .5 )
+    X <- cumsum(X) / (1:1000)
+    plot( 1:1000, X, type="l", ylim=c(.3,.7), col=ifelse(colour,emphCol,emphGrey),
+          xlab = "Número de tiradas", ylab = "Proporción de Caras", lwd=3
+    )
+    abline(h=.5,lty=2,col=emphGrey,lwd=2)
+
+  }
+  par(def.par)#- reset to default
+
+  # print
+  if( png ) makePng( paste0(directory, paste0("probability/",fileName)), width, height )
+
+
+}
+frequentistProb()
+
+## -- fig-4pantsprob ------
+
+set.seed(42)
+pantsDist <- function() {
+
+  # plots the bar graph showing the "pants" probability distribution
+
+  # needed for printing
+  fileName <- "pantsDistribution_es.png"
+  width <- 8
+  height <- 6
+
+  # key information
+  probabilities <- c( .5, .3, .1, 0, .1)
+  eventNames <- c( "Jean azul", "Jean gris", "Jean negro",
+                   "Pant. traje", "Pant. deportivo" )
+
+  # draw the plot
+  barplot(
+    height= probabilities,
+    xlab = "Evento",
+    ylab = "Probabilidad del evento",
+    names.arg = eventNames,
+    density = 10,
+    col = ifelse(colour,emphCol,emphGrey)
+  )
+
+  # print
+  if( png ) makePng( paste0(directory, paste0("probability/",fileName)), width, height )
+}
+pantsDist()
+
+
+## fig-4binomial1 ------
+
+
+binomExamples <- function() {
+
+  # plots the three examples of a binomial distribution
+
+  # needed for printing
+  width <- 8
+  height <- 6
+
+  # function to produce a styled binomial plot
+  binomPlot <- function( n,p, ... ) {
+
+    # probabilities of each outcome
+    out <- 0:n
+    prob <- dbinom( x=out, size=n, prob=p )
+
+    # plot
+    plot(
+      out, prob, type="h", lwd=3, ylab="Probabilidad",
+      frame.plot=FALSE, col=ifelse(colour,emphCol,"black"), ...
+    )
+
+  }
+
+  # skulls image...
+  binomPlot( n=20, p=1/6, xlab="Number of skulls observed" )
+  if( gif ) makeGif( paste0(dir, "binomSkulls20_es.gif"), width, height )
+
+  # coins image #1...
+  binomPlot( n=20, p=1/2, xlab="Number of heads observed" )
+  if( gif ) makeGif( paste(dir, "binomHeads20_es.gif", sep=""), width, height )
+
+  # coins image #2...
+  binomPlot( n=100, p=1/2, xlab="Number of heads observed" )
+  if( gif ) makeGif( paste(dir, "binomHeads100_es.gif", sep=""), width, height )
+}
+
